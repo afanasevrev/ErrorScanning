e75980799a1e5b8dac1e0dac48c77ac97a638cd0
revoly afanasev <rewoly@mail.ru>
2025-04-08 22:40:06 +0900

    first commit

--- src/main/java/com/example/ErrorScanning/ErrorScanningApplication.java
+++ src/main/java/com/example/ErrorScanning/ErrorScanningApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ErrorScanningApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ErrorScanningApplication.class, args);
 	}
-
 }
