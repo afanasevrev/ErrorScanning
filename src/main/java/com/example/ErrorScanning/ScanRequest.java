ec0c6e78f6f6b44c4bef29513e84ffe9a5fa60d1
revoly afanasev <rewoly@mail.ru>
2025-04-08 22:53:10 +0900

    first commit

---
+++ src/main/java/com/example/ErrorScanning/ScanRequest.java
@@ -0,0 +1,13 @@
+package com.example.ErrorScanning;
+/**
+ * Класс запроса
+ */
+public class ScanRequest {
+    private String url;
+    public String getUrl() {
+        return url;
+    }
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
---
+++ src/main/java/com/example/ErrorScanning/ScanResult.java
@@ -0,0 +1,22 @@
+package com.example.ErrorScanning;
+
+import java.util.List;
+/**
+ * Класс результата
+ */
+public class ScanResult {
+    private String url;
+    private List<String> vulnerabilities;
+    public String getUrl() {
+        return url;
+    }
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    public List<String> getVulnerabilities() {
+        return vulnerabilities;
+    }
+    public void setVulnerabilities(List<String> vulnerabilities) {
+        this.vulnerabilities = vulnerabilities;
+    }
+}
