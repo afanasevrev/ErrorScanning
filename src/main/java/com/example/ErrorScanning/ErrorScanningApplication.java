d863c9fdcdd891546b020c1a8ba3d70dde55ef5f
revoly afanasev <rewoly@mail.ru>
2025-04-13 17:14:13 +0900

    Добавим в проект БД

--- src/main/java/com/example/ErrorScanning/ErrorScanningApplication.java
+++ src/main/java/com/example/ErrorScanning/ErrorScanningApplication.java
@@ -1,11 +1,17 @@
 package com.example.ErrorScanning;
 
+import com.example.ErrorScanning.db.VulnerabilityService;
+import com.example.ErrorScanning.db.VulnerabilityServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ErrorScanningApplication {
+	@Bean
+	VulnerabilityService vulnerabilityService() {
+		return new VulnerabilityServiceImpl();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ErrorScanningApplication.class, args);
 	}
